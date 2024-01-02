43e9705caf27a4e368e7bea68f8781962dd6e242
Yuvraj Maurya <yuvraj95166@gmail.com>
2024-01-02 20:05:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+//your JS code here. If required.
+// JavaScript logic for audio playback
+const soundsDir = 'sounds/';
+
+let audioElements = [];
+
+function playSound(soundName) {
+  stopAllSounds();
+  const audio = new Audio(soundsDir + soundName);
+  audio.play();
+  audioElements.push(audio);
+}
+
+function stopAllSounds() {
+  audioElements.forEach(audio => {
+    audio.pause();
+    audio.currentTime = 0;
+  });
+  audioElements = [];
+}
+
+function pauseSound() {
+    audioElements.forEach(audio => {
+        audio.pause();
+        audio.cuurenTime = 0;
+    });
+    audioElements = [];
+}
